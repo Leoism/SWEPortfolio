1c7688a627ac66d91b50dee9d43c537f3ecff5b2
Leonardo Mota <contact@leoism.me>
2021-07-02 20:30:12 -0700

    Move from .sass files to .scss files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoryListModule } from './components/category-list/category-list.module';
 import { ExperienceOverviewModule } from './pages/experience-overview/experience-overview.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ExperienceOverviewModule } from './pages/experience-overview/experience
     AppRoutingModule,
     CategoryListModule,
     ExperienceOverviewModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
