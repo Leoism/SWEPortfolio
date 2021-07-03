27a5e8a0bd12010e00c5713656e9e556719c1ec3
Leonardo Mota <contact@leoism.me>
2021-07-03 11:32:06 -0700

    Make overview use mat-expansion-panel

--- src/app/pages/experience-overview/experience-overview.module.ts
+++ src/app/pages/experience-overview/experience-overview.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { ExperienceOverview } from './experience-overview.component';
+import { MatExpansionModule } from '@angular/material';
 
 import { CategoryListModule } from '../../components/category-list/category-list.module';
+
+import { ExperienceOverview } from './experience-overview.component';
 
 @NgModule({
   declarations: [ExperienceOverview],
   imports: [
     CategoryListModule,
     CommonModule,
+    MatExpansionModule,
   ],
   exports: [ExperienceOverview],
 })
