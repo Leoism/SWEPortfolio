6b9dd6a426fbbba11318403e4c9d9b7e04c3440f
Leonardo Mota <contact@leoism.me>
2021-07-13 14:04:25 -0700

    Create an about me card + Create card that displays a profile image, entries provided, and url buttons

--- src/app/pages/about-overview/about-overview.component.ts
+++ src/app/pages/about-overview/about-overview.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
 
+interface AboutEntry {
+  title: string; // title of the entry
+  value: string; // text related to the title
+}
+
+interface AboutUrl {
+  url: string; // url to redirect to 
+  image: string; // image url to display
+  alt?: string; // alt text for the image
+}
+
 @Component({
   selector: 'about-overview',
   templateUrl: './about-overview.component.html',
   styleUrls: ['./about-overview.component.scss']
 })
 export class AboutOverview {
+  aboutEntries: AboutEntry[] = [];
+  profileImage: string = '';
+  aboutUrls: AboutUrl[] = [];
 }
--- src/app/pages/about-overview/about-overview.module.ts
+++ src/app/pages/about-overview/about-overview.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { AboutOverview } from './about-overview.component';
-
 
 
 @NgModule({
   declarations: [AboutOverview],
   imports: [
     CommonModule,
+    MatCardModule,
   ],
   exports: [AboutOverview],
 })
